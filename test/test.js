7d843d66e3584e97cd8a798db73677f83572076e
xushengfeng <xushengfeng_zg@163.com>
2023-07-14 17:59:01 +0800

    测试 更新

--- test/test.js
+++ test/test.js
@@ -8,15 +8,31 @@ async function start() {
         dev: true,
         node: true,
     });
+    await x.initText({ textPath: "./m/vit-b-16.txt.fp32.onnx", dev: true, node: true });
     let img = document.createElement("img");
-    img.src = "../x.webp";
+    img.src = "../a9.png";
     img.onload = () => {
         let canvas = document.createElement("canvas");
         canvas.width = img.width;
         canvas.height = img.height;
         canvas.getContext("2d").drawImage(img, 0, 0);
+        let l = [];
         x.img(canvas.getContext("2d").getImageData(0, 0, img.width, img.height)).then((data) => {
-            
+            for (let i of ["杰尼龟", "妙蛙种子", "小火龙", "皮卡丘"]) {
+                x.text(i).then((tdata) => {
+                    let r = x.r(data, tdata);
+                    l.push(r);
+                    let t = 0;
+                    for (let i of l) {
+                        t += Math.E ** i;
+                    }
+                    let nl = [];
+                    for (let i of l) {
+                        nl.push(Math.E ** i / t);
+                    }
+                    console.log(i, nl);
+                });
+            }
         });
     };
 }
